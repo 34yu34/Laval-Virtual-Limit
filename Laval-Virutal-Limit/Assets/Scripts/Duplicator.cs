c84db6be7e22c00106a1c5ee2b5fd1665ebab907
jacob643 <jacobdorais25@hotmail.com>
2021-07-05 08:05:25 -0400

    duplicator

---
+++ Laval-Virutal-Limit/Assets/Scripts/Duplicator.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// is throwable and frozen, but when taken, duplicate itself.
+public class Duplicator : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private bool duplicated = false;
+    private Vector3 init_pos;
+    private Quaternion init_rot;
+
+    public void Start()
+    {
+        init_pos = transform.position;
+        init_rot = transform.rotation;
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true;
+    }
+
+    public void OnGrab()
+    {
+        if(duplicated)
+        {
+            return;
+        }
+        duplicate();
+    }
+
+    public void OnDrop()
+    {
+        allow_movement();
+    }
+
+    private void duplicate()
+    {
+        var next_duplicator = Instantiate<GameObject>(gameObject);
+        set_init_params(next_duplicator);
+        duplicated = true;
+    }
+
+    private void set_init_params(GameObject next_duplicator)
+    {
+        next_duplicator.name = gameObject.name;
+        next_duplicator.transform.position = init_pos;
+        next_duplicator.transform.rotation = init_rot;
+    }
+
+    private void allow_movement()
+    {
+        _rb.isKinematic = false;
+    }
+}
