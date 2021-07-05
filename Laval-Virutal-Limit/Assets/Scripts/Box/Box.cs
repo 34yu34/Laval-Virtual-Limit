4612c6741f71d6b2882cf19e5598f150097c2ba1
Audrey Germain <audrey.germain@polymtl.ca>
2021-07-05 14:10:51 -0400

    add taping mechanics

--- Laval-Virutal-Limit/Assets/Scripts/Box/Box.cs
+++ Laval-Virutal-Limit/Assets/Scripts/Box/Box.cs
@@ -17,12 +17,18 @@ public class Box : MonoBehaviour
     [SerializeField]
     GameObject _taped_box_prefab;
 
+    TapedPoint[] taped_points;
+
     GameObject _current_box;
 
     // Start is called before the first frame update
     void Start()
     {
         _current_box = Instantiate(_opened_box_prefab);
+        taped_points = GetComponentsInChildren<TapedPoint>();
+
+        SetPointActive(false);
+
         set_position_box();
         _state = BoxState.opened;
     }
@@ -37,14 +43,19 @@ public class Box : MonoBehaviour
     {
         Destroy(_current_box);
         _current_box = Instantiate(_closed_box_prefab, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+
+        SetPointActive(true);
+
         set_position_box();
         _state = BoxState.closed;
     }
+
 
     public void ChangeStateTaped()
     {
         Destroy(_current_box);
         _current_box = Instantiate(_taped_box_prefab, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+        SetPointActive(false);
         set_position_box();
         _state = BoxState.taped;
     }
@@ -55,5 +66,13 @@ public class Box : MonoBehaviour
         _current_box.transform.localPosition = Vector3.zero;
         _current_box.transform.localRotation = Quaternion.identity;
         _current_box.transform.localScale = Vector3.one;
+    }
+
+    private void SetPointActive(bool val)
+    {
+        foreach (var pt in taped_points)
+        {
+            pt.gameObject.SetActive(val);
+        }
     }
 }
