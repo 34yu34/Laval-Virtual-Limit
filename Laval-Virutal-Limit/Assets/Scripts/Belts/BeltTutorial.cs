d742b6670131bb9a470e1999d129b9d0b86bbf7d
Audrey Germain <audrey.germain@polymtl.ca>
2021-07-05 22:18:11 -0400

    Add tape 2

---
+++ Laval-Virutal-Limit/Assets/Scripts/Belts/BeltTutorial.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltTutorial : Belt
+{
+    [SerializeField]
+    private float _timestamps_stop = 10;
+
+    private bool _stopped = false;
+    private void Start()
+    {
+        _next_object_spawn_timestamp = int.MaxValue;
+    }
+
+
+}
