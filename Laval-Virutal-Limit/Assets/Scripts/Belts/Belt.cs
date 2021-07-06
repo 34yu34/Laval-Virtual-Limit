ab9516323ec8b492c4cf6a3fffbee4613d434014
Audrey Germain <audrey.germain@polymtl.ca>
2021-07-05 23:33:48 -0400

    add end of scene in belt

--- Laval-Virutal-Limit/Assets/Scripts/Belts/Belt.cs
+++ Laval-Virutal-Limit/Assets/Scripts/Belts/Belt.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Belt : MonoBehaviour
 {
@@ -17,19 +18,41 @@ public class Belt : MonoBehaviour
 
     private float _next_object_spawn_timestamp;
 
+    private float _last_object_spawn_timestamp = 0;
+
+    [SerializeField]
+    private float _delay_at_the_end;
+
+    [SerializeField]
+    private string _next_scene;
+
+    public int ItemsLeft => _wrappable_to_spawn.Length - _curr_index;
 
     private void FixedUpdate()
     {
-        if (_next_object_spawn_timestamp < Time.fixedTime)
+        if (_next_object_spawn_timestamp < Time.fixedTime && _last_object_spawn_timestamp == 0)
         {
             _next_object_spawn_timestamp = _time_between_object + Time.fixedTime;
 
             select_next_item();
+        }
+        if(_last_object_spawn_timestamp != 0)
+        {
+            if(Time.fixedTime - _last_object_spawn_timestamp > _delay_at_the_end)
+            {
+                SceneManager.LoadScene(_next_scene);
+            }
         }
     }
 
     private void select_next_item()
     {
         _spawner.ItemToSpawn = _wrappable_to_spawn[_curr_index++ % _wrappable_to_spawn.Length];
+        if (_curr_index >= _wrappable_to_spawn.Length)
+        {
+            _last_object_spawn_timestamp = Time.fixedTime;
+
+        }
+
     }
 }
--- Laval-Virutal-Limit/Assets/Scripts/Belts/BeltTutorial.cs
+++ Laval-Virutal-Limit/Assets/Scripts/Belts/BeltTutorial.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class BeltTutorial : Belt
 {
-    [SerializeField]
-    private float _timestamps_stop = 10;
+    //[SerializeField]
+    //private float _timestamps_stop = 10;
 
-    private bool _stopped = false;
-    private void Start()
-    {
-        _next_object_spawn_timestamp = int.MaxValue;
-    }
+    //private bool _stopped = false;
+    //private void Start()
+    //{
+    //    _next_object_spawn_timestamp = int.MaxValue;
+    //}
 
 
 }
